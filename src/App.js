2d6bc5bc07e8f78beb59091ce367fcf169520dba
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-05 11:30:01 +0300

    Navigation pages created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Register, Landing, Error } from './pages';
+import { Dashboard, Register, Landing, Error } from './NavigationPages';
 function App() {
     return (
         <BrowserRouter>
